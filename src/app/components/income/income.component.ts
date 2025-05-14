114ecc9961e6985f0e0ed4d46306f48ff2064e13
mkkotlin <123.ultron.1234@gmail.com>
2025-05-14 13:20:46 +0530

    Forms Added

--- src/app/components/income/income.component.ts
+++ src/app/components/income/income.component.ts
@@ -1,21 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { Income, IncomeService } from '../../services/income.service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-income',
-  imports: [CommonModule, FormsModule, MatCardModule],
+  imports: [CommonModule, FormsModule, MatCardModule, ReactiveFormsModule],
   templateUrl: './income.component.html',
   styleUrl: './income.component.css'
 })
 export class IncomeComponent implements OnInit{
   private incomeService = inject(IncomeService);
   incomes: Income[] = [];
+  private fb = inject(FormBuilder);
+  incomeForm!: FormGroup;
   ngOnInit() {
     this.incomeService.getIncome().subscribe((data)=>{
       this.incomes = data;
+    })
+
+    this.incomeForm = this.fb.group({
+      source: ['', Validators.required],
+      amount: ['',[Validators.required, Validators.min(1)]],
+      date: ['',Validators.required],
+      notes: ['']
+
     })
 
   }
@@ -33,10 +43,18 @@ export class IncomeComponent implements OnInit{
   successMessage=''
   errorMessage=''
   submitIncome(){
-    this.incomeService.addIncome(this.newIncome).subscribe({
+
+    if(this.incomeForm.invalid){ this.incomeForm.markAllAsTouched();
+      this.errorMessage = "Please fill required field";
+      return;
+    }
+
+    const formData = this.incomeForm.value;
+    this.incomeService.addIncome(formData).subscribe({
       next:(res)=>{
-        this.successMessage = 'Income addes';
+        this.successMessage = 'Income addedd';
         this.errorMessage ='';
+        this.incomeForm.reset();
       this.newIncome = {
             source: '',
             amount: '',
